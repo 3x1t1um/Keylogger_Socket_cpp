cbe7979db0785f7a8fb6eb1224e3d7b0b63515e6
Arthur Pons <unguestdev@gmail.com>
2020-09-23 13:46:49 +0200

    Logging received info
    Putting received keys into a log file which named by the address of the socket.
    Using open() in append mode to avoid the deletion trough ancient connections.

--- server.py
+++ server.py
@@ -27,5 +27,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # (wtf)
     conn, addr = s.accept()
     
     while 1:
-        data = conn.recv(1024)
-        print(' '+data.decode('utf-8'))
+        with open(f'{addr}.txt', 'a+') as file:
+            data = conn.recv(1024).decode('utf-8')
+            print(' '+data)
+            file.write(data)
