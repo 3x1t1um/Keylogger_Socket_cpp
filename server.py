cd09da2a783c9dc42482939ed5cbbc7b5f41d209
Arthur Pons <unguestdev@gmail.com>
2020-09-23 16:11:12 +0200

    Update server.py
    Adding timestamp to the log file.

--- server.py
+++ server.py
@@ -28,6 +28,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # (wtf)
     
     while 1:
         with open(f'{addr}.txt', 'a+') as file:
+         
             data = conn.recv(1024).decode('utf-8')
             print(' '+data)
-            file.write(data)
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            file.write(f'{dt_string} : {data}')
